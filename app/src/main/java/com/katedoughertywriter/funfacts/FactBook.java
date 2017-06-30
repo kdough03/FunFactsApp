5dd932d3c5d20db32799440834dd7865ff3d4e39
Kate-at-Clover <kate@clover.com>
2017-06-30 12:08:47 -0600

    Edited fact about Captain Picard.

--- app/src/main/java/com/katedoughertywriter/funfacts/FactBook.java
+++ app/src/main/java/com/katedoughertywriter/funfacts/FactBook.java
@@ -22,7 +22,7 @@ public class FactBook {
             "Some penguins can leap 2-3 meters out of the water.",
             "On average, it takes 66 days to form a new habit.",
             "Mammoths still walked the earth when the Great Pyramid was being built.",
-            "Captain Picard is the best of all Star Trek captains." };
+            "Captain Picard is the best of all the Star Trek captains." };
 
     /**
      * This method has no parameters.
