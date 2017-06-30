cea37cb898a7dae12c0ea3033eb1efdf33df5493
Kate-at-Clover <kate@clover.com>
2017-06-30 12:06:17 -0600

    Added fact about Captain Picard.

--- app/src/main/java/com/katedoughertywriter/funfacts/FactBook.java
+++ app/src/main/java/com/katedoughertywriter/funfacts/FactBook.java
@@ -21,7 +21,8 @@ public class FactBook {
             "The state of Florida is bigger than England.",
             "Some penguins can leap 2-3 meters out of the water.",
             "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built." };
+            "Mammoths still walked the earth when the Great Pyramid was being built.",
+            "Captain Picard is the best of all Star Trek captains." };
 
     /**
      * This method has no parameters.
